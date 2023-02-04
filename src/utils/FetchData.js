e02625e062166d85e109ea92d0bab835a866f758
abkardev <ammg.11@hotmail.com>
2023-02-04 19:12:16 +0300

    update keys

--- src/utils/FetchData.js
+++ src/utils/FetchData.js
@@ -1,7 +1,7 @@
 export const exercisesOptions = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': '5e62698e5bmsh09bbf73b87f4991p1a03d2jsnd2078789a6d6',
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
       'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
     }
   };
@@ -9,7 +9,7 @@ export const exercisesOptions = {
    export const youtubeOptions = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': '5e62698e5bmsh09bbf73b87f4991p1a03d2jsnd2078789a6d6',
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
       'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
     }
   };
